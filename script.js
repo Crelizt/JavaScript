2e52006fce5b031f22da2f234f476d74e65ac1c6
Crelizt <f.ecalvin.cf@gmail.com>
2021-04-07 11:06:47 +0700

    Init String Concat

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log(1)
+const user = document.getElementById('user');
+
+//ES 5
+const user_logged_in = "Calvin"
+// user.innerHTML = "Selamat Datang, " + user_logged_in;
+
+//ES 6
+user.innerHTML = `Anda berhasil login ${user_logged_in}`;
