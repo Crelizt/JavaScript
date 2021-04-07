ee85b61f369caf02115680f3041001d9e0342237
Crelizt <f.ecalvin.cf@gmail.com>
2021-04-07 11:21:40 +0700

    inisialisasi object literal

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log(1)
+//Object Literals
+const cart = document.getElementById("cart")
+
+//ES5
+function addProduct(name, category,price){
+    return{
+        name: name,
+        category: category,
+        productPrice: price
+    }
+}
+
+var getProduct = addProduct("iPhone 12", "Smartphone", 100000);
+console.log(getProduct)
+
+cart.innerHTML = `Product 1: ${getProduct.name}, category ${getProduct.category}, price ${getProduct.productPrice}`
+
+
+
+//ES6
+
+function addProduct(name, category, price){
+    return{name, category, productPrice:price};
+}
+
+var getProduct = addProduct("iPhone 12", "Smartphone", 100000);
+console.log(getProduct)
+
+cart.innerHTML = `Product 1: ${getProduct.name}, category ${getProduct.category}, price ${getProduct.productPrice}}`
