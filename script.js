b3c3fa340572397d0eb5a54d793c8cf278ca51c6
Crelizt <f.ecalvin.cf@gmail.com>
2021-04-07 13:20:45 +0700

    init map & filter

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log(1)
+const skills = document.getElementById("skills");
+const primarySkill = document.getElementById("primarySkill");
+
+const mySkills = ["ES6", "Git", "Web Development"]
+
+
+//Map untuk pindahin isi array ke suatu array baru dengan isinya yang uda di modif (jika ada)
+const printSkills = mySkills.map((skill) => {
+    return skill;
+})
+
+skills.innerHTML = printSkills;
+
+
+//Filter untuk pindahin isi array ke suatu array baru dengan ada constraint yang isinya sesuai constraint.
+const printPrimarySkill = mySkills.filter((skill) => {
+    return skill != "ES6";
+})
+
+primarySkill.innerHTML = printPrimarySkill;
