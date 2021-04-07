2fa360907fa2af4c80934c15701997e8bf46b0fa
Crelizt <f.ecalvin.cf@gmail.com>
2021-04-07 11:48:11 +0700

    init foreach

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log(1)
+const skills = document.getElementById("skills");
+
+const mySkills = ["ES6", "UX Design", "Git"];
+
+mySkills.push("Dev Ops");
+
+console.log(mySkills);
+
+
+//Cara 1
+function showSkill(mySkill){
+    console.log(mySkill);
+}
+
+mySkills.forEach(showSkill);
+console.log("------------");
+
+
+//Cara 2
+mySkills.forEach((mySkill) => {
+    console.log(mySkill)
+})
+console.log("------------");
+
+//Cara 3
+const showSkill2 = (mySkill) => {
+    console.log(mySkill);
+}
+
+mySkills.forEach(showSkill2);
+
+
+//Ditampilin di HTML
+let parent = '<ul>';
+
+mySkills.forEach((mySkill) => {
+    parent += `<li>${mySkill}</li>`;
+});
+
+parent += "</ul>";
+skills.innerHTML = parent;
+
